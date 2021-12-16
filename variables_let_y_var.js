18511012e7177b479a9fe88f2e36c0020914da56
Enmanuel Vallejo Caicedo <95440695+DONVALLEJO@users.noreply.github.com>
2021-12-15 23:41:55 -0500

    Add files via upload

---
+++ variables_let_y_var.js
@@ -0,0 +1,28 @@
+var a=1;
+let b=2;
+console.log("valor a inicial");
+console.log(a);
+console.log("valor b inicial");
+console.log(b);
+
+
+
+if(a>0){
+var a=11;
+let b=22;
+console.log("valor a en el if");
+console.log(a);
+console.log("valor b en el if");
+console.log(b);
+
+
+}
+console.log("valor a fuera de el if");
+console.log(a);
+console.log("valor b fuera el if");
+console.log(b);
+
+
+/*conclusion la variable var es global y siempre toma el ultima valor que se le asigne
+y la variable lef solo cambia el valor en un  bloque de sentencia pero luego vuelve a su valor inicial
+*/
