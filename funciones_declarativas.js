35c8d3210d5eac1ed0646b417746cf568d960054
Enmanuel Vallejo Caicedo <95440695+DONVALLEJO@users.noreply.github.com>
2021-12-17 00:25:44 -0500

    Add files via upload

---
+++ funciones_declarativas.js
@@ -0,0 +1,17 @@
+function suma(v1,v2){
+	
+	return v1+v2;	
+}
+const sum =suma(2,5)
+console.log('LA SUMA DE V1+V2 ES  '+sum) 
+document.write('LA SUMA DE V1+V2 ES:  '+sum)
+
+
+function resta(n1,n2) {
+	
+	return `LA DIFERENCIA DE N1 Y N2 ES: ${n1-n2}`
+	
+}
+const res = resta(3,7)
+console.log(res)
+document.write("<br>"+res)
